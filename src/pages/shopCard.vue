ab92bcd35ae508206159cc5c8e50f0216e234958
e <616858970@qq.com>
2018-07-02 17:56:29 +0800

    ‘添加页面shopCard

---
+++ src/pages/shopCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+         <shopList />
+    </div>
+</template>
+
+<script>
+  import shopList from '@/components/shopList'
+    export default {
+        name:'shopCard',
+        data() {
+            return {
+                key: ''
+            }
+        },
+        components:{
+            shopList,
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+const ShopCard =  ()=>import(/* webpackChunkName: "group-foo" */'@/pages/shopCard');
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/shopCard',
+      name: 'shopCard',
+      component: ShopCard
     }
   ]
 })
